919fa04aa5b53e3b0ecd264ff7ff20689e407330
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-30 21:26:47 +0100

    oppo6893: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: alps Model: oppo6893 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ omni_oppo6893.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from oppo6893 device
+$(call inherit-product, device/alps/oppo6893/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := oppo6893
+PRODUCT_NAME := omni_oppo6893
+PRODUCT_BRAND := alps
+PRODUCT_MODEL := oppo6893
+PRODUCT_MANUFACTURER := alps
